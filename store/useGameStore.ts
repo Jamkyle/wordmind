8b8e682577e0250e9b3470fcee03fe168a26a54b
Aro Rahari <arorahari@MacBook-Pro-de-Aro.local>
2025-03-04 14:25:57 +0100

    chore(zustand): implement store zustand

--- store/useGameStore.ts
+++ store/useGameStore.ts
@@ -1,7 +1,68 @@
-import { PlayerState } from "@/libs/models/player";
+import { Game, GameAction, GamesAction, GameState } from "@/libs/models/game";
+import {
+  Player,
+  PlayerAction,
+  PlayersState,
+  PlayerState,
+} from "@/libs/models/player";
+import { Room, RoomAction } from "@/libs/models/room";
+import { randomName } from "@/libs/utils/playerUtils";
 import { create } from "zustand";
 
-export const useGameStore = create<PlayerState>((set) => ({
-  players: [],
-  setPlayers: (players) => set({ players }),
+export const usePlayerStore = create<PlayerState & PlayerAction>((set) => ({
+  id: null,
+  name: randomName?.(),
+  score: 0,
+  isReady: false,
+  setPlayer: (player: Player) => set({ ...player }),
+  setPlayerName: (name: string) => set({ name }),
+  setPlayerId: (id: string) => set({ id }),
+  setPlayerScore: (score: number) => set({ score }),
+  toggleLocalReady: () => set((state) => ({ isReady: !state.isReady })),
+  resetPlayer: () => set({ name: randomName(), score: 0, isReady: false }),
+}));
+
+export const usePlayersStore = create<PlayersState>((set) => ({
+  playersByRoom: {},
+  setPlayers: (roomId, players) =>
+    set((state) => ({
+      playersByRoom: {
+        ...state.playersByRoom,
+        [roomId]: players,
+      },
+    })),
+  markPlayerReady: (roomId, playerId) =>
+    set((state) => ({
+      playersByRoom: {
+        ...state.playersByRoom,
+        [roomId]: state.playersByRoom[roomId]?.map((player) =>
+          player.id === playerId
+            ? { ...player, isReady: player.isReady }
+            : player
+        ),
+      },
+    })),
+}));
+
+export const useGameStore = create<Omit<Room, "id"> & GameAction & GameState>((set) => ({
+  wordToGuess: "",
+  createdAt: new Date(),
+  currentTurn: 0,
+  guesses: [],
+  status: "waiting",
+  setGameStatus: (status) => set({ status }),
+  setGameData: (data) => set((state) => ({ ...state, ...data })),
+}));
+
+export const useGamesStore = create<Game & GamesAction>((set) => ({
+  gameStatusByRooms: [],
+  setGameStatusByRooms: (roomId, status) =>
+    set((state) => ({
+      gameStatusByRooms: state.gameStatusByRooms.map((game) => {
+        if (game.id === roomId) {
+          return { ...game, status };
+        }
+        return game;
+      }),
+    })),
 }));
